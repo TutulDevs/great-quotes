8f0da6ce0ac230d13bbfadcfd8f1304412adc5a6
Tutul <tutulnahid@gmail.com>
2021-07-05 20:48:12 +0600

    added React.lazy for components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import React, { Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import NewQuote from "./pages/NewQuote";
-import Page404 from "./pages/Page404";
-import QuoteDetail from "./pages/QuoteDetail";
-import Quotes from "./pages/Quotes";
+import Loader from "./components/UI/Loader";
+
+//import NewQuote from "./pages/NewQuote";
+//import Page404 from "./pages/Page404";
+//import QuoteDetail from "./pages/QuoteDetail";
+//import Quotes from "./pages/Quotes";
+
+// Lazy components
+const Quotes = React.lazy(() => import("./pages/Quotes"));
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const Page404 = React.lazy(() => import("./pages/Page404"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes' />
-        </Route>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes' />
+          </Route>
 
-        <Route path='/quotes' component={Quotes} exact />
-        <Route path='/quotes/:quoteId' component={QuoteDetail} />
-        <Route path='/new-quote' component={NewQuote} />
+          <Route path='/quotes' component={Quotes} exact />
+          <Route path='/quotes/:quoteId' component={QuoteDetail} />
 
-        <Route path='*' component={Page404} />
-      </Switch>
+          <Route path='/new-quote' component={NewQuote} />
+
+          <Route path='*' component={Page404} />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
